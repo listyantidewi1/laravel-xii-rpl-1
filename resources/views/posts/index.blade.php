bf2a8bf1e34e84d9cadc92907e14a02b78bf29ca
listyantidewi1 <listyantidewi@gmail.com>
2023-02-06 09:53:52 +0700

    tambah data

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,10 +1,10 @@
 {{-- menggunakan template dari file app.blade.php di folder layouts --}}
 @extends('layouts.app')
 {{-- memberi judul pada posisi 'judul' --}}
-@section('judul', 'All blog posts')
+@section('title', 'All blog posts')
 {{-- menampilkan semua posts pada posisi isi --}}
-@section('isi')
-    @forelse ($post as $key => $post)
+@section('content')
+    @forelse ($posts as $key => $post)
         {{ $key }} . {{ $post['title'] }} <br > {{ $post['content'] }}
         <br/>
     @empty
